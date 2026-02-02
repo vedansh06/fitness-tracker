dbfde59fa469ea1d79369beb5b5cde3b9f2eb77f
Vedansh Saini <vedanshsaini06@gmail.com>
2026-02-02 19:29:41 +0530

    feat: add ThemeContext for managing theme state and toggling

---
+++ client/src/context/ThemeContext.tsx
@@ -0,0 +1,28 @@
+import { createContext, useContext, useState } from "react";
+
+interface ThemeContextType {
+  theme: String;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [theme, setTheme] = useState(
+    () =>
+      localStorage.getItem("theme") ||
+      (window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light"),
+  );
+
+  return <ThemeContext.Provider value={}>{children}</ThemeContext.Provider>;
+}
+
+export function useTheme() {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+  return context;
+}
