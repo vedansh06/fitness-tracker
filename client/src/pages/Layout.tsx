b78646e1369b225762a492bb2eec8a3841996177
Vedansh Saini <vedanshsaini06@gmail.com>
2026-02-02 19:09:49 +0530

    feat: implement routing and add UI components for user interaction

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,5 +1,12 @@
+import { Outlet } from "react-router-dom";
+
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <div>
+      Layout
+      <Outlet />
+    </div>
+  );
 };
 
 export default Layout;
