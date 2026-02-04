6106918f626739c3ad01b5553b06809c4a763e88
Vedansh Saini <vedanshsaini06@gmail.com>
2026-02-04 19:34:52 +0530

    Onboarding pages for user notifications

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -8,6 +8,7 @@ import {
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAppContext } from "../context/AppContext";
+import { Toaster } from "react-hot-toast";
 
 const Login = () => {
   const [state, setState] = useState("login");
@@ -39,6 +40,7 @@ const Login = () => {
 
   return (
     <>
+      <Toaster />
       <main className="login-page-container">
         <form onSubmit={handleSubmit} className="login-form">
           <h2 className="text-3xl font-medium text-gray-900 dark:text-white">
--- client/src/pages/Onboarding.tsx
+++ client/src/pages/Onboarding.tsx
@@ -1,5 +1,57 @@
+import { PersonStanding } from "lucide-react";
+import { useState } from "react";
+import { Toaster } from "react-hot-toast";
+import { useAppContext } from "../context/AppContext";
+import type { ProfileFormData } from "../types";
+
 const Onboarding = () => {
-  return <div>Onboarding</div>;
+  const [step, setStep] = useState(1);
+  const { user, setOnboardingCompleted, fetchUser } = useAppContext();
+  const [formData, setFormData] = useState<ProfileFormData>({
+    age: 0,
+    weight: 0,
+    height: 0,
+    goal: "maintain",
+    dailyCalorieIntake: 2000,
+    dailyCalorieBurn: 400,
+  });
+
+  const totalSteps = 3;
+
+  return (
+    <>
+      <Toaster />
+      <div className="onboarding-container">
+        {/* Header */}
+        <div className="p-6 pt-12 onboarding-wrapper">
+          <div className="flex items-center gap-3 mb-2">
+            <div className="w-10 h-10 rounded-xl bg-emerald-500 flex items-center justify-center">
+              <PersonStanding className="w-6 h-6 text-white" />
+            </div>
+            <h1 className="text-2xl font-bold text-slate-800 dark:text-white">
+              FitTrack
+            </h1>
+          </div>
+          <p>Let's personalize your experience</p>
+        </div>
+
+        {/* Progress Indicator */}
+        <div className="px-6 mb-8 onboarding-wrapper">
+          <div className="flex gap-2 max-w-2xl">
+            {[1, 2, 3].map((s) => (
+              <div
+                key={s}
+                className={`h-1.5 flex-1 rounded-full transition-all duration-300 ${s <= step ? "bg-emerald-500" : "bg-slate-200 dark:bg-slate-800"}`}
+              />
+            ))}
+          </div>
+          <p className="text-sm text-slate-400 mt-3">
+            Step {step} of {totalSteps}{" "}
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Onboarding;
