3f07736978fdd2c15883917ef02acecd8ab125c8
Vedansh Saini <vedanshsaini06@gmail.com>
2026-02-04 10:57:21 +0530

    feat: implement loading component and enhance login form submission handling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,12 +6,18 @@ import ActivityLog from "./pages/ActivityLog";
 import Profile from "./pages/Profile";
 import { useAppContext } from "./context/AppContext";
 import Login from "./pages/Login";
+import Loading from "./components/Loading";
+import Onboarding from "./pages/Onboarding";
 
 const App = () => {
   const { user, isUserFetched, onboardingCompleted } = useAppContext();
 
   if (!user) {
-    return isUserFetched ? <Login /> : <p>Loading</p>;
+    return isUserFetched ? <Login /> : <Loading />;
+  }
+
+  if (!onboardingCompleted) {
+    return <Onboarding />;
   }
 
   return (
