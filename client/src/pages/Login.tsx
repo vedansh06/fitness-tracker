2e1d2e37b21850d3fbf0abf6021f5f1c5f0e333d
Vedansh Saini <vedanshsaini06@gmail.com>
2026-02-04 10:08:32 +0530

    feat: implement login form with username, email, and password fields; add password visibility toggle

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,113 @@
+import {
+  AtSignIcon,
+  EyeIcon,
+  EyeOffIcon,
+  LockIcon,
+  MailIcon,
+} from "lucide-react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAppContext } from "../context/AppContext";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [state, setState] = useState("sign up");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const navigate = useNavigate();
+  const { login, signup, user } = useAppContext();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    }
+  }, [user, navigate]);
+
+  return (
+    <>
+      <main className="login-page-container">
+        <form className="login-form">
+          <h2 className="text-3xl font-medium text-gray-900 dark:text-white">
+            {state === "login" ? "Sign In" : "Sign Up"}
+          </h2>
+          <p className="mt-2 text-sm text-gray-500/90 dark:text-gray-400">
+            {state === "login"
+              ? "Please enter email and password to access."
+              : "Please enter your details to create an account."}
+          </p>
+
+          {/* Username */}
+          {state !== "login" && (
+            <div className="mt-4">
+              <label className="font-medium text-sm text-gray-700 dark:text-gray-300">
+                Username
+              </label>
+              <div className="relative mt-2">
+                <AtSignIcon className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 size-4.5" />
+                <input
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  type="text"
+                  placeholder="enter a username"
+                  className="login-input"
+                  required
+                />
+              </div>
+            </div>
+          )}
+
+          {/* Email */}
+          <div className="mt-4">
+            <label className="font-medium text-sm text-gray-700 dark:text-gray-300">
+              Email
+            </label>
+            <div className="relative mt-2">
+              <MailIcon className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 size-4.5" />
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                placeholder="Please enter your email"
+                className="login-input"
+                required
+              />
+            </div>
+          </div>
+
+          {/* Password */}
+          <div className="mt-4">
+            <label className="font-medium text-sm text-gray-700 dark:text-gray-300">
+              Password
+            </label>
+            <div className="relative mt-2">
+              <LockIcon className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 size-4.5" />
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Please enter your password"
+                className="login-input pr-10"
+                type={showPassword ? "text" : "password"}
+                required
+              />
+              <button
+                type="button"
+                className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600 "
+                onClick={() => setShowPassword((p) => !p)}>
+                {showPassword ? (
+                  <EyeOffIcon size={16} />
+                ) : (
+                  <EyeIcon size={16} />
+                )}
+              </button>
+            </div>
+          </div>
+        </form>
+      </main>
+    </>
+  );
 };
 
 export default Login;
