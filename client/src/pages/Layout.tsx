1e0c22a9f12581926174243d841b49883dca27a7
Vedansh Saini <vedanshsaini06@gmail.com>
2026-02-05 13:19:07 +0530

    feat: add Sidebar component and integrate it into the Layout

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
 
 const Layout = () => {
   return (
-    <div>
-      Layout
-      <Outlet />
+    <div className="layout-container">
+      <Sidebar />
+      <div className="flex-1 overflow-y-scroll">
+        <Outlet />
+      </div>
     </div>
   );
 };
