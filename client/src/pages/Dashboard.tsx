d2a1fb515a9f2bfee7ce8a966764c31192a61e90
Vedansh Saini <vedanshsaini06@gmail.com>
2026-02-05 18:05:54 +0530

    implement Dashboard component

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,5 +1,79 @@
+import { useEffect, useState } from "react";
+import { getMotivationalMessage } from "../assets/assets";
+import { useAppContext } from "../context/AppContext";
+import type { ActivityEntry, FoodEntry } from "../types";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { user, allActivityLogs, allFoodLogs } = useAppContext();
+
+  const [todayFood, setTodayFood] = useState<FoodEntry[]>([]);
+  const [todayActivities, setTodayActivities] = useState<ActivityEntry[]>([]);
+
+  const DAILY_CALORIE_LIMIT: number = user?.dailyCalorieIntake || 2000;
+
+  // Load user data
+  const loadUserData = () => {
+    const today = new Date().toISOString().split("T")[0];
+
+    const foodData = allFoodLogs.filter(
+      (f: FoodEntry) => f.createdAt?.split("T")[0] === today,
+    );
+    setTodayFood(foodData);
+
+    const activityData = allActivityLogs.filter(
+      (a: ActivityEntry) => a.createdAt?.split("T")[0] === today,
+    );
+    setTodayActivities(activityData);
+  };
+
+  useEffect(() => {
+    (() => {
+      loadUserData();
+    })();
+  }, [allActivityLogs, allFoodLogs]);
+
+  const totalCalories: number = todayFood.reduce(
+    (sum, item) => sum + item.calories,
+    0,
+  );
+
+  const remainingCalories: number = DAILY_CALORIE_LIMIT - totalCalories;
+
+  const totalActiveMinutes: number = todayActivities.reduce(
+    (sum, item) => sum + item.duration,
+    0,
+  );
+
+  const totalBurned: number = todayActivities.reduce(
+    (sum, item) => sum + (item.calories || 0),
+    0,
+  );
+
+  const motivation = getMotivationalMessage(
+    totalCalories,
+    totalActiveMinutes,
+    DAILY_CALORIE_LIMIT,
+  );
+
+  return (
+    <div className="page-container">
+      {/* Header */}
+
+      <div className="dashboard-header">
+        <p className="text-emerald-100 text-sm font-medium">Welcome back</p>
+        <h1 className="text-2xl font-bold mt-1">{`Hi there! 👋 ${user?.username}`}</h1>
+
+        {/* Motivation Card */}
+
+        <div className="mt-6 bg-white/20 backdrop-blur-sm rounded-2xl p-4">
+          <div className="flex items-center gap-3">
+            <span className="text-3xl">{motivation.emoji}</span>
+            <p className="text-white font-medium">{motivation.text}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
