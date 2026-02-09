1c795d6d6d1fb406f23de75ba91ac7f2dd42ab7f
Vedansh Saini <vedanshsaini06@gmail.com>
2026-02-09 20:32:18 +0530

    feat: implement create functionality in ActivityLog controller with user association

--- server/src/api/activity-log/controllers/activity-log.ts
+++ server/src/api/activity-log/controllers/activity-log.ts
@@ -4,4 +4,24 @@
 
 import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController("api::activity-log.activity-log");
+export default factories.createCoreController(
+  "api::activity-log.activity-log",
+  ({ strapi }) => ({
+    async create(ctx) {
+      const user = ctx.state.user;
+
+      if (!user) return ctx.unauthorized("Login required");
+      const body = ctx.request.body.data;
+      body.users_permissions_user = user.id;
+
+      const entry = await strapi.entityService.create(
+        "api::activity-log.activity-log",
+        {
+          data: body,
+          populate: ["users_permissions_user"],
+        },
+      );
+      return entry;
+    },
+  }),
+);
