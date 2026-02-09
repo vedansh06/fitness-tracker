394d50aa060a9bfc13c3b61f91c9e2ac2e073025
Vedansh Saini <vedanshsaini06@gmail.com>
2026-02-09 20:24:48 +0530

    initialize Strapi for create a backend server

---
+++ server/src/api/activity-log/controllers/activity-log.ts
@@ -0,0 +1,7 @@
+/**
+ * activity-log controller
+ */
+
+import { factories } from "@strapi/strapi";
+
+export default factories.createCoreController("api::activity-log.activity-log");
---
+++ server/src/api/food-log/controllers/food-log.ts
@@ -0,0 +1,7 @@
+/**
+ * food-log controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::food-log.food-log');
